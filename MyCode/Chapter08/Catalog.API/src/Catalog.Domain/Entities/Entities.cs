b095acdea39bf0e093452e4299b234d75a7a22ad
Brian Walsh <brian.walsh@ruby.com>
2021-02-08 17:29:30 -0800

    moved around entities and repositories.

---
+++ MyCode/Chapter08/Catalog.API/src/Catalog.Domain/Entities/Entities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Domain.Entities
+{
+    //Artist.cs file
+    public class Artist
+    {
+        public Guid ArtistId { get; set; }
+        public string ArtistName { get; set; }
+    }
+    //Genre.cs file
+    public class Genre
+    {
+        public Guid GenreId { get; set; }
+        public string GenreDescription { get; set; }
+    }
+    //Price.cs file
+    public class Price
+    {
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
---
+++ MyCode/Chapter08/Catalog.API/src/Catalog.Domain/Entities/IItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Domain.Entities;
+
+namespace Catalog.Domain.Repositories
+{
+    public interface IItemRepository : IRepository
+    {
+        Task<IEnumerable<Item>> GetAsync();
+        Task<Item> GetAsync(Guid id);
+        Item Add(Item item);
+        Item Update(Item item);
+    }
+}
